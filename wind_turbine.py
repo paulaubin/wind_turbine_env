6cd4c51f947fa22f001c31c576c7f8c3f9f72f46
paaubin <paaubin@mac-x17tl0yjnf.lan>
2022-09-26 21:21:00 +0200

    simplify the wind model with a single step duration as parameter

--- wind_turbine.py
+++ wind_turbine.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 from math_utils import wrap_to_m180_p180
 
 class Wind_turbine:
-
 	def __init__(self):
 		pass
 
@@ -35,13 +34,15 @@ class Wind_turbine:
 # Model of the wind
 class Wind:
 	model = 'OU' 					# model used for wind simulation
-	__dt = 1 						# s, represents the standard time step
 	__c_speed_factor = 0.2 			# variable used in the OU model
 	__c_heading_factor = 0.005 		# variable used in the OU model
 	__speed_target = 0.0 			# m/s, represents the average wind speed on some minutes
 	__heading_target = 0.0 			# deg, represents the average wind angle on some minutes
+	__diurnal_period = 24*3600 		# s, represents the period of the diurnal cycle
+	__diurnal_speed_factor = 0.2 	# represents the wind speed loss on a diurnal cycle
+	__time = 0 						# s, represent the elapsed time in s due to the cumulative steps
 
-	def __init__(self, initial_speed=None, initial_heading=None, model_type='OU'):
+	def __init__(self, initial_speed=None, initial_heading=None, step_duration=None, model_type='OU'):
 		''' heading is the wind angle wrt Northin degree
 			speed is in m/s. A negative speed would result in a 180° shift in heading
 			model_type represents the model used to simulate the wind, the 'OU'
@@ -50,6 +51,7 @@ class Wind:
 		'''
 		self._speed = 0 if initial_speed is None else initial_speed
 		self._heading = 0 if initial_heading is None else initial_heading
+		self.__step_duration = self.__dt if step_duration is None else step_duration
 		self.model_type = model_type
 
 		# Initialise hidden variables. The heading and speed target corresponds to the
@@ -58,7 +60,7 @@ class Wind:
 		self.__speed_target = self._speed
 		self.__heading_target = self._heading
 
-	def step(self, step_duration=1, model='OU'):
+	def step(self, model='OU'):
 		'''
 		step_duration must be an int
 		'''
@@ -66,13 +68,13 @@ class Wind:
 			mean_sp = self._speed
 			mean_hd = self._heading
 			steps = 1
-			for i in range(int(np.ceil(step_duration/self.__dt))):
+			for i in range(int(np.ceil(self.__step_duration))):
 				# Compute fast chaning wind at 1/dt frequency, typically 1Hz
-				self.ou()
+				self.__ou()
 				steps += 1
 				mean_sp += (self._speed - mean_sp)/steps
 				mean_hd += (self._heading - mean_hd)/steps
-				if i % step_duration == 1:
+				if i % self.__step_duration == 1:
 					# Flush new value
 					self._speed = mean_sp
 					self._heading = mean_hd
@@ -83,9 +85,9 @@ class Wind:
 		else:
 			print('Wind model not found in class ', str(self.__class__))
 
-	def ou(self):
+	def __ou(self):
 		'''
-		This is the Ornstein-Uhlenbeck precess, a stationary Gauss-Markov model,
+		This is the Ornstein-Uhlenbeck process, a stationary Gauss-Markov model,
 		a math definition is available at page 7 here : https://www.merl.com/publications/docs/TR2022-102.pdf
 		'''
 		c_speed = self.__c_speed_factor * self.__speed_target
@@ -95,6 +97,13 @@ class Wind:
 
 		c_heading = self.__c_heading_factor * self.__heading_target
 		self._heading = c_heading + (1 - self.__c_heading_factor) * self._heading + np.random.normal(0.0, 0.3)
+
+	def __diurnal_cycle(self):
+		'''
+		The diurnal cycles is modelised as a periodic day-night shift of the wind speed
+		'''
+		pass
+		
 
 	@property
 	def heading(self):
@@ -111,8 +120,8 @@ class Wind:
 		return str(self.__class__) + ": " + str(self.__dict__)
 		
 
-w1 = Wind(-10, 90, 'OU')
-w2 = w1
+w1 = Wind(10, 0, 1, 'OU')
+w2 = Wind(10, 0, 10, 'OU')
 
 time1 = np.linspace(0, 3600, 3601)
 w1_sp_log = np.zeros((np.size(time1), 1))
@@ -120,7 +129,7 @@ w1_h_log = np.zeros((np.size(time1), 1))
 for t in range(len(time1)) :
 	w1_sp_log[t] = w1.speed
 	w1_h_log[t] = w1.heading
-	w1.step(1)
+	w1.step()
 
 time2 = np.linspace(0, 3600, 361)
 w2_sp_log = np.zeros((np.size(time2), 1))
@@ -128,7 +137,7 @@ w2_h_log = np.zeros((np.size(time2), 1))
 for t in range(len(time2)) :
 	w2_sp_log[t] = w2.speed
 	w2_h_log[t] = w2.heading
-	w2.step(10)
+	w2.step()
 
 print('mean speed 1= ', np.mean(w1_sp_log))
 print('std speed 1= ', np.std(w1_sp_log))

9a3f5b5928431ecab884401818fa7efb7c73e812
POPOS <sagar@pop-os.localdomain>
2022-10-02 13:44:30 +0530

    small changes to the documentation

--- math_utils.py
+++ math_utils.py
@@ -10,4 +10,7 @@
 import numpy as np
 
 def wrap_to_m180_p180(angle_in_degree : float) -> float :
+	"""
+	Wrap an angle in degree to [-180, 180]
+	"""
 	return np.mod(angle_in_degree + 180, 360) - 180
--- simu.py
+++ simu.py
@@ -76,3 +76,4 @@ class Simu:
 
 	def __str__(self):
 		return str(self.__class__) + ": " + str(self.__dict__)
+
